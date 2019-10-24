221cab7c70497e3d2c87e28acd9ca10cdaed3f47
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-24 14:19:01 +0200

    clase principal

---
+++ diasemanacorrespondiente/DiaSemanaCorrespondiente.java
@@ -0,0 +1,35 @@
+package diasemanacorrespondiente;
+
+import java.util.*;
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class DiaSemanaCorrespondiente {
+
+    
+    public static void main(String[] args) {
+        // dias de la semana
+    Scanner sc=new Scanner(System.in);
+     Dias dia=new Dias();
+        
+     
+     System.out.println("Dime un número");
+    int valor=sc.nextInt();
+    
+    dia.diaSemana(valor);
+        
+    dia.diaSwitch(valor);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
